7302963d4f3e1880794eb7b8698aa07e7b4a329e
baran yalçın <baran.yalcin@ogr.sakarya.edu.tr>
2024-04-29 10:24:54 +0300

    added log4j2 for management service

--- management/src/main/java/tyt/management/controller/UserController.java
+++ management/src/main/java/tyt/management/controller/UserController.java
@@ -2,6 +2,7 @@ package tyt.management.controller;
 
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.*;
 import tyt.management.controller.request.CreateUserRequest;
 import tyt.management.controller.request.UpdateUserRequest;
@@ -11,15 +12,19 @@ import tyt.management.service.UserService;
 
 import java.util.List;
 
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/user")
+@Log4j2
 public class UserController {
 
     private final UserService userService;
 
     @GetMapping("/{id}")
     public UserDTO getUser(@PathVariable Long id) {
+
+        log.info("Getting user with id: {}", id);
         return userService.getUser(id);
     }
 
@@ -30,12 +35,15 @@ public class UserController {
 
     @PostMapping("/create")
     public String createUser(@RequestBody CreateUserRequest request) {
+        log.info("Creating user with name: {}", request.getName());
         return userService.createUser(UserMapper.INSTANCE.createRequestToDto(request));
     }
 
 
     @PutMapping("/update")
     public String updateUser(@RequestBody UpdateUserRequest request) {
+
+        log.info("Updating user with id: {}", request.getId());
         return userService.updateUser(UserMapper.INSTANCE.updateRequestToDto(request));
     }
 
@@ -45,6 +53,7 @@ public class UserController {
         userDTO.setId(id);
         userDTO.setActive(false);
         userService.deleteUser(userDTO);
+        log.info("Deleting user with id: {}", id);
     }
 
 }
