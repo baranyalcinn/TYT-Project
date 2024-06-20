8d640c9e6f34b5dd9586ea6472dc1c9967d5fb06
baranyalcinn <baran.yalcin@ogr.sakarya.edu.tr>
2024-06-20 23:24:13 +0300

    test for jenkins pipeline

--- eureka/src/main/java/tyt/eureka/EurekaApplication.java
+++ eureka/src/main/java/tyt/eureka/EurekaApplication.java
@@ -20,5 +20,5 @@ public class EurekaApplication {
     public static void main(String[] args) {
         SpringApplication.run(EurekaApplication.class, args);
     }
-
+//test for jenkins pipeline
 }
