8c3c357c1f892f27657de697ce4157217c77029f
baranyalcinn <baran.yalcin@ogr.sakarya.edu.tr>
2024-06-22 21:02:52 +0300

    .

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -9,16 +9,31 @@ pipeline {
         stage('Detect Changes') {
             steps {
                 script {
-                    def changedDirs = sh(returnStdout: true, script: 'git diff --name-only HEAD^ HEAD').trim().split("\n")
-                    echo "Changed directories: ${changedDirs.join(', ')}"
+                    // Get the list of changed files
+                    def changedFiles = sh(returnStdout: true, script: 'git diff --name-only HEAD^ HEAD').trim().split("\n")
+                    echo "Changed files: ${changedFiles.join(', ')}"
 
-                    for (dir in changedDirs) {
-                        if (fileExists("${dir}/pom.xml")) {
-                            echo "Change detected in directory with pom.xml: ${dir}"
-                            buildDockerImage(dir)
-                        } else {
-                            echo "No pom.xml found in ${dir}, skipping..."
+                    def changedDirsWithPom = []
+                    for (file in changedFiles) {
+                        def currentDir = file.substring(0, file.lastIndexOf('/'))
+                        while (currentDir != '') {
+                            if (fileExists("${currentDir}/pom.xml")) {
+                                echo "Change detected in directory with pom.xml: ${currentDir}"
+                                changedDirsWithPom.add(currentDir)
+                                break // Stop searching once pom.xml is found
+                            }
+                            // Go up one directory level
+                            currentDir = currentDir.substring(0, currentDir.lastIndexOf('/'))
+                            if (currentDir == '/') {
+                                break // Reached the root directory
+                            }
                         }
+                    }
+                    changedDirsWithPom = changedDirsWithPom.unique()
+
+                    // Build images for each unique directory containing pom.xml
+                    for (dir in changedDirsWithPom) {
+                        buildDockerImage(dir)
                     }
                 }
             }
